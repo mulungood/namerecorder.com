324866c1f54cdcf5404446b29c90edc345a8bd30
Henrique Doro <opensource@hdoro.dev>
2022-10-24 12:55:04 -0300

    chore: start SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<!-- Goal: record audio file based on microphone and have it ready for uploading -->
+
+<!-- WONTDO today: uploading itself, nice UI, API routing to existing recordings, auth - PURELY CLIENT_SIDE -->
